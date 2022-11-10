46e0bd56471294f9f9d7c61ced866aa08ab2f7ca
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-11-10 09:50:42 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= list.length - 1; index++) {
+        basic.showNumber(list[index])
+    }
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let list: string[] = []
+list = [
+"Billlie",
+"Le Sserafim",
+"IVE",
+"Nnmix",
+"G-idle",
+"Iz*one"
+]
+list = []
+let length = 0
+basic.showNumber(length)
